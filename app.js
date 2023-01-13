7972d11eace1c8d12b5907e33737d29adfa1c4dc
RisingTonklaman <21398@dusitaram.ac.th>
2023-01-13 23:52:13 +0700

    Add P'F
    ...

--- app.js
+++ app.js
@@ -2,18 +2,19 @@ const chalk = require("chalk");
 const morgan = require("morgan");
 const debug = require("debug");
 const express = require("express");
-
-const app = express()
-const port =  3000
+const path = require('path');
+const app = express();
+const port =  3000;
 
 app.use(morgan('combined')); 
+app.use(express.static(path.join(__dirname,'/Public/')))
 
 app.get('/' , (req,res) => {
-
     res.send('Authorised Rising Hopper!!!')
 
 
 })
+
 app.listen(port, () => {
 
     console.log('listening on port '+chalk.green(port));
--- chalk.js
+++
@@ -1,1 +0,0 @@
-console.log(require("chalk").green('port'));
